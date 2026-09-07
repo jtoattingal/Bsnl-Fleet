d5d87cb19242f65d978ed525eb03219f4dc6809f
jtoattingal <jto.venjaramoodu@gmail.com>
2026-09-07 20:13:36 +0530

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -6,24 +6,4 @@ const serverModule = require('../dist/server.cjs');
 
 const expressApp = serverModule.default || serverModule;
 
-const handler = serverless(expressApp);
-
-export default async function (req: any, res: any) {
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
-  // Prevent serverless function from waiting indefinitely
-  return new Promise((resolve, reject) => {
-    handler(req, res).then(resolve).catch(reject);
-  });
-}
+export default serverless(expressApp);
